6da0380c5ea9a229f87ae317303e7947282585ff
maxx52 <gospelwork@yandex.ru>
2025-05-13 09:11:14 +0700

    RA-04 update MainActivity

--- app/src/main/java/ru/maxx52/androidsprint/CategoriesListFragment.kt
+++ app/src/main/java/ru/maxx52/androidsprint/CategoriesListFragment.kt
@@ -38,15 +38,4 @@ class FragmentListCategories : Fragment() {
         super.onDestroyView()
         _binding = null
     }
-
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FragmentListCategories().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
--- app/src/main/java/ru/maxx52/androidsprint/MainActivity.kt
+++ app/src/main/java/ru/maxx52/androidsprint/MainActivity.kt
@@ -1,6 +1,5 @@
 package ru.maxx52.androidsprint
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import ru.maxx52.androidsprint.databinding.ActivityMainBinding
@@ -10,17 +9,16 @@ class MainActivity : AppCompatActivity() {
     private val binding
         get() = _binding ?: throw IllegalStateException("Binding не доступен или null")
 
-    @SuppressLint("CommitTransaction")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         if (savedInstanceState == null) {
-            val fragmentTransaction = supportFragmentManager.beginTransaction()
-            fragmentTransaction.apply {
+            supportFragmentManager.beginTransaction().apply {
                 setReorderingAllowed(true)
                 add(R.id.fragmentListCategories, FragmentListCategories())
+                commit()
             }
         }
     }
