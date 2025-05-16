80139d5fcccda5f522832827eec586b9ed1af823
maxx52 <gospelwork@yandex.ru>
2025-05-16 22:15:46 +0700

    RA-05 Set up basic button navigation

--- app/src/main/java/ru/maxx52/androidsprint/FavoritesFragment.kt
+++ app/src/main/java/ru/maxx52/androidsprint/FavoritesFragment.kt
@@ -8,18 +8,9 @@ import android.view.ViewGroup
 import ru.maxx52.androidsprint.databinding.FragmentFavoritesBinding
 
 class FavoritesFragment : Fragment() {
-    private var btnCategory: String? = null
-    private var btnFavorites: String? = null
     private var _binding: FragmentFavoritesBinding? = null
-    private val binding get() = _binding ?: throw IllegalStateException("View is not initialized")
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            btnCategory = it.getString(ARG_CATEGORY)
-            btnFavorites = it.getString(ARG_FAVORITES)
-        }
-    }
+    private val binding
+        get() = _binding ?: throw IllegalStateException("View is not initialized")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
