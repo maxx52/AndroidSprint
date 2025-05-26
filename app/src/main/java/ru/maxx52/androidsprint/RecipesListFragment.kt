2aa9e40c004bbadd0d93ac873137ab1c9f79a2b8
maxx52 <gospelwork@yandex.ru>
2025-05-26 22:49:51 +0700

    RA-11 Implement a screen with a list of recipes

--- app/src/main/java/ru/maxx52/androidsprint/RecipesListFragment.kt
+++ app/src/main/java/ru/maxx52/androidsprint/RecipesListFragment.kt
@@ -5,16 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.commit
 import ru.maxx52.androidsprint.databinding.FragmentRecipesListBinding
+import ru.maxx52.androidsprint.entities.STUB
+
+const val ARG_RECIPE_ID = "recipeId"
 
 class RecipesListFragment : Fragment() {
     private var _binding: FragmentRecipesListBinding? = null
-    private val binding
-        get() = _binding ?: throw IllegalStateException("View is not initialized")
+    private val binding get() = _binding ?: throw IllegalStateException("View is not initialized")
 
-    var categoryId: Int? = null
-    var categoryName: String? = null
-    var categoryImageUrl: String? = null
+    private var categoryId: Int? = null
+    private var categoryName: String? = null
+    private var categoryImageUrl: String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,15 +30,34 @@ class RecipesListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val args = arguments
-        if (args != null) {
-            categoryId = args.getInt(ARG_CATEGORY_ID, -1)
-            categoryName = args.getString(ARG_CATEGORY_NAME) ?: "Без названия"
-            categoryImageUrl = args.getString(ARG_CATEGORY_IMAGE_URL) ?: ""
-        } else {
-            categoryId = -1
-            categoryName = "Без названия"
-            categoryImageUrl = ""
+        arguments?.let {
+            categoryId = it.getInt(ARG_CATEGORY_ID, -1)
+            categoryName = it.getString(ARG_CATEGORY_NAME) ?: "Без названия"
+            categoryImageUrl = it.getString(ARG_CATEGORY_IMAGE_URL) ?: ""
+        }
+        initRecycler()
+    }
+
+    private fun initRecycler() {
+        val categoriesAdapter = CategoriesListAdapter(STUB.getCategories())
+        binding.rvRecipes.adapter = categoriesAdapter
+
+        categoriesAdapter.setOnItemClickListener(object : CategoriesListAdapter.OnItemClickListener {
+            override fun onItemClick(categoryId: Int) {
+                openRecipeByRecipeId(categoryId)
+            }
+        })
+    }
+
+    fun openRecipeByRecipeId(recipeId: Int) {
+        val recipeFragment = RecipeFragment().apply {
+            arguments = Bundle().apply {
+                putInt(ARG_RECIPE_ID, recipeId)
+            }
+        }
+        parentFragmentManager.commit {
+            setReorderingAllowed(true)
+            replace(R.id.mainContainer, recipeFragment)
         }
     }
 
