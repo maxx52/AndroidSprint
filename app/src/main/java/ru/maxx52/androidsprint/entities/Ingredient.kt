0f3dec12e9e565f7f0cea7e0d47ae4444075f101
maxx52 <gospelwork@yandex.ru>
2025-06-04 17:30:36 +0700

    RA-12 apply @Parcelize for data-classes

--- app/src/main/java/ru/maxx52/androidsprint/entities/Ingredient.kt
+++ app/src/main/java/ru/maxx52/androidsprint/entities/Ingredient.kt
@@ -1,37 +1,11 @@
 package ru.maxx52.androidsprint.entities
 
-import android.os.Parcel
+import kotlinx.parcelize.Parcelize
 import android.os.Parcelable
 
+@Parcelize
 data class Ingredient(
     val quantity: String,
     val unitOfMeasure: String,
     val description: String,
-) : Parcelable {
-
-    constructor(parcel: Parcel) : this(
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readString() ?: ""
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(quantity)
-        parcel.writeString(unitOfMeasure)
-        parcel.writeString(description)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Ingredient> {
-        override fun createFromParcel(parcel: Parcel): Ingredient {
-            return Ingredient(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Ingredient?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+) : Parcelable
--- app/src/main/java/ru/maxx52/androidsprint/entities/Recipe.kt
+++ app/src/main/java/ru/maxx52/androidsprint/entities/Recipe.kt
@@ -1,43 +1,13 @@
 package ru.maxx52.androidsprint.entities
 
-import android.os.Parcel
+import kotlinx.parcelize.Parcelize
 import android.os.Parcelable
 
+@Parcelize
 data class Recipe(
     val id: Int,
     val title: String,
     val imageUrl: String,
     private val ingredients: List<Ingredient>,
     private val method: List<String>,
-) : Parcelable {
-
-    constructor(parcel: Parcel) : this(
-        parcel.readInt(),
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.createTypedArrayList(Ingredient.CREATOR) ?: emptyList(),
-        parcel.createStringArrayList() ?: emptyList()
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeInt(id)
-        parcel.writeString(title)
-        parcel.writeString(imageUrl)
-        parcel.writeTypedList(ingredients)
-        parcel.writeStringList(method)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Recipe> {
-        override fun createFromParcel(parcel: Parcel): Recipe {
-            return Recipe(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Recipe?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+) : Parcelable
