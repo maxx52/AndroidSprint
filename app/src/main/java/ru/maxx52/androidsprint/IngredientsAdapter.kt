4559fc3b16b26a33b9d2ecac1d64eec85a90da47
maxx52 <gospelwork@yandex.ru>
2025-06-15 19:13:38 +0700

    RA-14 add binding and optimize code

--- app/src/main/java/ru/maxx52/androidsprint/IngredientsAdapter.kt
+++ app/src/main/java/ru/maxx52/androidsprint/IngredientsAdapter.kt
@@ -2,42 +2,36 @@ package ru.maxx52.androidsprint
 
 import android.annotation.SuppressLint
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import ru.maxx52.androidsprint.databinding.ItemIngredientsBinding
 import ru.maxx52.androidsprint.entities.Ingredient
 import java.math.BigDecimal
+import java.math.RoundingMode
 
 class IngredientsAdapter(
     private var dataSet: List<Ingredient>
 ) : RecyclerView.Adapter<IngredientsAdapter.ViewHolder>() {
     private var quantity: BigDecimal = BigDecimal.ONE
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val titleTextView: TextView = view.findViewById(R.id.tvIngredientName)
-        val quantityTextView: TextView = view.findViewById(R.id.tvIngredientCount)
-    }
+    inner class ViewHolder(val binding: ItemIngredientsBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(viewGroup.context)
-            .inflate(R.layout.item_ingredients, viewGroup, false)
-        return ViewHolder(view)
+        val binding = ItemIngredientsBinding.inflate(LayoutInflater
+            .from(viewGroup.context), viewGroup, false)
+        return ViewHolder(binding)
     }
 
-    @SuppressLint("DefaultLocale")
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
         val ingredient = dataSet[position]
-        val originalQuantity = ingredient.quantity.toBigDecimalOrNull() ?: BigDecimal.ZERO
-        val updatedQuantity = originalQuantity.multiply(quantity)
-        val displayQuantity = when {
-            updatedQuantity.remainder(BigDecimal.ONE) == BigDecimal.ZERO ->
-                updatedQuantity.toInt().toString()
-            else -> "%.1f".format(updatedQuantity)
-        }
+        val displayQuantity = (ingredient.quantity.toBigDecimalOrNull() ?: BigDecimal.ZERO)
+            .multiply(quantity)
+            .setScale(1, RoundingMode.HALF_UP)
+            .stripTrailingZeros()
+            .toPlainString()
         val displayText = "$displayQuantity ${ingredient.unitOfMeasure}"
-        viewHolder.titleTextView.text = ingredient.description
-        viewHolder.quantityTextView.text = displayText
+        viewHolder.binding.tvIngredientName.text = ingredient.description
+        viewHolder.binding.tvIngredientCount.text = displayText
     }
 
     override fun getItemCount() = dataSet.size
