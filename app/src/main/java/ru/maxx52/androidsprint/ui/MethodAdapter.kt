37107975e5149d68b06e7cf53c09fd8bce2046d3
maxx52 <gospelwork@yandex.ru>
2025-08-16 22:09:14 +0700

    RA-24 Take out the assets receipt and transfer the ready-made Drawable to the state

--- app/src/main/java/ru/maxx52/androidsprint/ui/MethodAdapter.kt
+++ app/src/main/java/ru/maxx52/androidsprint/ui/MethodAdapter.kt
@@ -3,28 +3,34 @@ package ru.maxx52.androidsprint.ui
 import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import ru.maxx52.androidsprint.databinding.ItemMethodBinding
 
-class MethodAdapter(private var dataSet: List<String>)
-    : RecyclerView.Adapter<MethodAdapter.ViewHolder>() {
+class MethodAdapter : ListAdapter<String, MethodAdapter.ViewHolder>(DIFF_CALLBACK) {
+
+    companion object {
+        private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<String>() {
+            override fun areItemsTheSame(oldItem: String, newItem: String): Boolean =
+                oldItem == newItem
+
+            override fun areContentsTheSame(oldItem: String, newItem: String): Boolean =
+                oldItem == newItem
+        }
+    }
 
     inner class ViewHolder(val binding: ItemMethodBinding) : RecyclerView.ViewHolder(binding.root)
 
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.binding.tvMethodItem.text = "${position + 1}. ${getItem(position)}"
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = ItemMethodBinding.inflate(LayoutInflater
-            .from(parent.context), parent, false)
+        val binding = ItemMethodBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
     }
 
-    @SuppressLint("SetTextI18n")
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.binding.tvMethodItem.text = "${position + 1}. ${dataSet[position]}"
-    }
-
-    override fun getItemCount() = dataSet.size
-
-    fun updateMethods(methods: List<String>) {
-        methods.also { dataSet = it }
-    }
+    override fun getItemCount(): Int = currentList.size
 }
