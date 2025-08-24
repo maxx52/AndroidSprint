cdd0d64c58a2b58524a3991372f992487ca8b2b6
maxx52 <gospelwork@yandex.ru>
2025-08-25 06:43:32 +0700

    RA-30

--- app/src/main/java/ru/maxx52/androidsprint/ui/FavoritesFragment.kt
+++ app/src/main/java/ru/maxx52/androidsprint/ui/FavoritesFragment.kt
@@ -6,8 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.commit
-import androidx.fragment.app.replace
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import ru.maxx52.androidsprint.R
