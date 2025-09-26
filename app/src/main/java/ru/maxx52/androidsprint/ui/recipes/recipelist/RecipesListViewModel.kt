be8c8bf0e2a46722fdc57d51652ae04abc36ed17
maxx52 <gospelwork@yandex.ru>
2025-09-26 15:18:30 +0700

    RA-36 cleanup code

--- app/src/main/java/ru/maxx52/androidsprint/ui/recipes/recipelist/RecipesListViewModel.kt
+++ app/src/main/java/ru/maxx52/androidsprint/ui/recipes/recipelist/RecipesListViewModel.kt
@@ -2,20 +2,14 @@ package ru.maxx52.androidsprint.ui.recipes.recipelist
 
 import android.app.Application
 import android.util.Log
-import android.widget.Toast
-import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
-import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
-import androidx.navigation.fragment.findNavController
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import ru.maxx52.androidsprint.data.NON_RECIPE
 import ru.maxx52.androidsprint.data.repository
 import ru.maxx52.androidsprint.model.Recipe
-import ru.maxx52.androidsprint.ui.RecipesListFragmentDirections
 
 class RecipesListViewModel(application: Application) : AndroidViewModel(application) {
 
